adcff88044a46baa4ab9e086675ac7b18cf17713
LePichu <ishat.prsnl65844@gmail.com>
2021-01-30 17:47:32 +0530

    test

--- src/main/java/com/remodstudios/mechwarf/items/ItemRegistry.java
+++ src/main/java/com/remodstudios/mechwarf/items/ItemRegistry.java
@@ -2,23 +2,27 @@ package com.remodstudios.mechwarf.items;
 
 import com.remodstudios.mechwarf.MechanicalWarfare;
 import com.remodstudios.mechwarf.blocks.BlockRegistry;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ItemRegistry {
+
+    private static final ItemGroup GROUP = FabricItemGroupBuilder.build(new Identifier(MechanicalWarfare.MOD_ID,"group"), () -> new ItemStack(ItemRegistry.PLUTONIUM_BLOCK));
+
     // items go BRRRRRRRRR
-    public static final Item CHARGED_REDSTONE_DUST = register(new ChargedItem(new Item.Settings().group(ItemGroup.MISC)), "charged_redstone_dust");
-
-
+    public static final Item CHARGED_REDSTONE_DUST = register(new ChargedItem(new Item.Settings().group(GROUP)), "charged_redstone_dust");
+    public static final Item PLUTONIUM_SHARD = register(new Item(new Item.Settings().group(GROUP)), "plutonium_shard");
 
     // Bwockitems go hewe
-    public static final Item CHARGED_REDSTONE_BLOCK = register(BlockRegistry.CHARGED_REDSTONE_BLOCK, "charged_redstone_block", new Item.Settings().group(ItemGroup.MISC));
-    public static final Item PLUTONIUM_ORE = register(BlockRegistry.PLUTONIUM_ORE, "plutonium_ore", new Item.Settings().group(ItemGroup.MISC));
-    public static final Item PLUTONIUM_BLOCK = register(BlockRegistry.PLUTONIUM_BLOCK, "plutonium_block", new Item.Settings().group(ItemGroup.MISC));
+    public static final Item CHARGED_REDSTONE_BLOCK = register(BlockRegistry.CHARGED_REDSTONE_BLOCK, "charged_redstone_block", new Item.Settings().group(GROUP));
+    public static final Item PLUTONIUM_ORE = register(BlockRegistry.PLUTONIUM_ORE, "plutonium_ore", new Item.Settings().group(GROUP));
+    public static final Item PLUTONIUM_BLOCK = register(BlockRegistry.PLUTONIUM_BLOCK, "plutonium_block", new Item.Settings().group(GROUP));
 
     ////////////////////////////////////
 
